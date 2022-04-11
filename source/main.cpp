8eef2f27ea0763703231395f26ed28098677aea3
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-11 14:12:58 -0700

    Added skeleton of runGame to main.

--- source/main.cpp
+++ source/main.cpp
@@ -5,23 +5,30 @@
 #include "headers\gameData.h"
 #include "headers\baseScreen.h"
 
+void runGame(sf::RenderWindow*);
+
 int main()
 {
     gameData::initializeData();
     sf::RenderWindow window(sf::VideoMode(gameData::screenWidth, gameData::screenLength), "Extra Equestrial");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.display();
-    }
+    runGame(&window);
 
     return 0;
+}
+
+void runGame(sf::RenderWindow* window) {
+    //runs game loop
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+
+        window->clear();
+        window->display();
+    }
 }
