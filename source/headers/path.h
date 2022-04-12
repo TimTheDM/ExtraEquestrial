b79e00f7db9cddfdafd88631a0916551450ae6eb
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-12 12:19:37 -0700

    Added members and member prototypes in path.h

--- source/headers/path.h
+++ source/headers/path.h
@@ -1,5 +1,17 @@
 #ifndef PATH_H
 #define PATH_H
 
+const float PI = 3.141592;
+
+class path {
+    float x_offset;
+    float y_offset;
+    float angle;
+    float mutation;
+    int timer;
+    int time;
+public: 
+    path(int, float, float);
+};
 
 #endif //PATH_H
