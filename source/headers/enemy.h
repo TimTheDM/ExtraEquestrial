d2e59d1f8c27133547ff7bee272b215adc2a63de
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-12 12:34:06 -0700

    Filled in header of enemy object.

--- source/headers/enemy.h
+++ source/headers/enemy.h
@@ -1,5 +1,18 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "bullet.h"
+
+class enemy {
+    //class that stores data for an enemy object
+    sf::Texture* enemyTexture;
+    std::vector<Path>* path;
+    std::string type;
+public:
+    enemy(int, int, const std::string&, sf::Vector2f&);
+    ~enemy();
+}
 
 #endif //ENEMY_H
