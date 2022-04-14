93f339656d281644f88bb5c8ed2d8a03b8bb795f
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-14 13:09:19 -0700

    Added prototypes of Path members in Path.h

--- source/headers/path.h
+++ source/headers/path.h
@@ -1,6 +1,8 @@
 #ifndef PATH_H
 #define PATH_H
 
+#include <math.h>
+
 const float PI = 3.141592;
 
 class Path {
@@ -13,6 +15,10 @@ class Path {
     int time;
 public: 
     Path(int, float, float);
+    void incrementPath();
+    float retrieve_x_offset();
+    float retrieve_y_offset();
+    bool isDone();
 };
 
 #endif //PATH_H
