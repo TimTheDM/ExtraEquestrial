ed4ea150a1519f3908d73cdc12b5464371843adb
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-14 13:19:03 -0700

    Implemented constructor of Path.

--- source/path.cpp
+++ source/path.cpp
@@ -1,7 +1,12 @@
 #include "headers\Path.h"
+#include "math.h"
 
 Path::Path(int time, float angle, float mutation) {
-    this->time = time;
+    if (time < 1) this->time = 1;
+    else this->time = time;
+    this->timer = 0;
     this->mutation = mutation;
     this->angle = angle;
+    this->x_offset = cos(angle);
+    this->y_offset = -sin(angle);
 }
