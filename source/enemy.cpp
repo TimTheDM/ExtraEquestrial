d64538509bfcf578da5a416fa01d864e572dabb5
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-14 13:03:41 -0700

    Fixed error in enemy class constructor. Now accepts Path vector.

--- source/enemy.cpp
+++ source/enemy.cpp
@@ -1,8 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include "headers\path.h"
+#include "headers\Path.h"
 #include "headers\enemy.h"
 
-enemy::enemy(int xpos, int ypos, const std::string& type, sf::Vector2f* pathVec) {
+enemy::enemy(int xpos, int ypos, const std::string& type, std::vector<Path>* path) {
     //constructor for enemy type, will use above data to populate enemy object
 }
