8c1554e900554f5d8633404c560f46417f6b0e24
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-15 12:11:32 -0700

    Added prototypes of member functions to gameScreen.h

--- source/gameScreen.cpp
+++ source/gameScreen.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <vector>
 
 #include "headers\gameData.h"
 #include "headers\baseScreen.h"
 #include "headers\gameScreen.h"
+#include "headers\Enemy.h"
 
 screenType gameScreen::run(sf::RenderWindow* window) {
     //runs a single game loop of the game screen
-    sf::Event event;
-    while (window->pollEvent(event)) {
-        if (event.type == sf::Event::Closed) gameData::endProgram = true;
-    }
+    this->processInput(window);
     return game;
 }
 
@@ -27,4 +26,11 @@ gameScreen::gameScreen() {
 
 gameScreen::~gameScreen() {
     //destructor for game screen object.
+}
+
+void gameScreen::processInput(sf::RenderWindow* window) {
+    sf::Event event;
+    while (window->pollEvent(event)) {
+        if (event.type == sf::Event::Closed) gameData::endProgram = true;
+    }
 }
--- source/headers/gameScreen.h
+++ source/headers/gameScreen.h
@@ -2,13 +2,24 @@
 #define GAME_SCREEN_H
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include "baseScreen.h"
+#include "Enemy.h"
 
 class gameScreen : public baseScreen {
-
+    std::vector<Enemy>* enemies;
+    sf::Sprite background;
+    //std::vector<Bullet>* bullets;
+    //Player* player; 
 public:
     screenType run(sf::RenderWindow*);
     void draw(sf::RenderWindow*);
+    void processInput(sf::RenderWindow*);
+    void moveBackground();
+    void moveEnemies();
+    void moveBullets();
+    void movePlayer();
+    void checkCollision();
     gameScreen();
     ~gameScreen();
 };
