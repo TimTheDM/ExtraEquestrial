60c2cb5f9d74d4c359716df64e419f20fd0a7ebd
Timothy Smithlin <tbsmithlin@gmail.com>
2022-04-19 00:30:44 -0700

    Implemented constructor and destructor of player.

--- source/player.cpp
+++ source/player.cpp
@@ -2,13 +2,18 @@
 #include <SFML/Graphics.hpp>
 #include "headers\SpriteSheet.h"
 #include "headers\player.h"
+#include "headers\assets.h"
 
 Player::Player() {
     //constructor for player
+    this->playerSprite = new SpriteSheet(assets::playerSprite, 5, 5);
+    this->x_offset = 0;
+    this->y_offset = 0;
 }
 
 Player::~Player() {
     //destructor  for player
+    delete this->playerSprite;
 }
 
 void Player::movePlayer() {
