bedcfb36c21cfb4cc71bfb354e3253ae4c18daa4
Timothy Smithlin <tbsmithlin@gmail.com>
2022-05-02 23:33:36 -0700

    Added interpreter files.

---
+++ source/headers/interpreter.h
@@ -0,0 +1,13 @@
+#ifndef INTERPRET_H
+#define INTERPRET_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Path.h"
+#include "enemy.h"
+
+Enemy interpret(std::string levelName);
+
+#endif // INTERPRET_H
