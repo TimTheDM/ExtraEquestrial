a3d8dbac263075cfbbe32d3af6a8ef0b35d531a3
Timothy Smithlin <tbsmithlin@gmail.com>
2022-05-20 21:14:50 -0700

    gameData now stores level the game is on.

--- source/gameData.cpp
+++ source/gameData.cpp
@@ -3,10 +3,12 @@
 const int INITIAL_WIDTH = 480;
 const int INITIAL_LENGTH = 212;
 const int STARTING_LIVES = 3;
+const int STARTING_LEVEL = 1;
 
 int gameData::screenWidth = 0;
 int gameData::screenLength = 0;
 int gameData::lives = 0;
+int gameData::level = 0;
 bool gameData::endProgram;
 
 bool gameData::initializeData() {
@@ -14,6 +16,7 @@ bool gameData::initializeData() {
     gameData::screenWidth = INITIAL_WIDTH;
     gameData::screenLength = INITIAL_LENGTH;
     gameData::lives = STARTING_LIVES;
+    gameData::level = STARTING_LEVEL;
     gameData::endProgram = false;
     return true;
 }
